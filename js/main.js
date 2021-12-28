41f51948d270e5d0a79d6f2f9872fb8e6737859c
Patri <patri@malvidopazo.es>
2021-12-28 12:44:51 +0100

    add solved exercise and styles

--- js/main.js
+++ js/main.js
@@ -1,74 +1,114 @@
 "use strict";
 
-//Variables globales
-let counter = 1;
-let playerValue = "";
-
 const playButton = document.querySelector(".js_button");
 const selectPlay = document.querySelector(".js_select_play");
+const resetButton = document.querySelector(".js_reset_button");
+const result = document.querySelector('.js_result');
+const player = document.querySelector(".js_player_score");
+const computer = document.querySelector(".js_computer_score");
 
-const infoUser = document.querySelector(".js_infouser");
-const player = document.querySelector(".js_player");
-const computer = document.querySelector(".js_computer");
-const randomNumber = getRandom(10);
+let playerScore = 0;
+let computerScore = 0;
+let moves = 0;
 
-//Funciones
+//Funcion para generar numero aleatorio
 
-function getRandom(max) {
+function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-function getOptionComputer(randomNumber) {
+//Funcion aleatoria de la jugada del ordenador
+function getOptionComputer() {
   let optionPc = "";
-
-  if (randomNumber < 3) {
+  let number = getRandomNumber(9);
+  if (number <= 3) {
     optionPc = "rock";
-  } else if (randomNumber >= 3) {
+  } else if (number <= 6) {
     optionPc = "paper";
-  } else if (randomNumber > 6) {
+  } else if (number > 6) {
     optionPc = "scissors";
   }
-
   return optionPc;
 }
 
-function compareOptionPlayer(optionPc) {
-  playerValue = selectPlay.value;
+//Funcion para obtener la jugada del usuario
+function getSelectedPlay (){
+  return selectPlay.value;
+}
 
-  console.log(optionPc, playerValue);
-  if (optionPc === "rock" && playerValue === "rock") {
-    infoUser.innerHTML = "Empate";
-  } else if (optionPc === "rock" && playerValue === "scissors") {
-    infoUser.innerHTML = "¡Has perdido!";
-  } else if (optionPc === "rock" && playerValue === "paper") {
-    infoUser.innerHTML = "¡Has ganado!";
-  } else if (optionPc === "paper" && playerValue === "rock") {
-    infoUser.innerHTML = "¡Has perdido";
-  } else if (optionPc === "paper" && playerValue === "scissors") {
-    infoUser.innerHTML === "¡Has ganado!";
-  } else if (optionPc === "paper" && playerValue === "paper") {
-    infoUser.innerHTML = "Empate";
-  } else if (optionPc === "scissors" && playerValue === "rock") {
-    infoUser.innerHTML = "¡Has ganado";
-  } else if (optionPc === "scissors" && playerValue === "scissors") {
-    infoUser.innerHTML = "Empate";
-  } else if (optionPc === "scissors" && playerValue === "paper") {
-    infoUser.innerHTML = "Has perdido";
+//Funcion para comparar las jugadas del usuario y del ordenador
+function playGame(){
+  let moveComputer = getOptionComputer();
+  let movePlayer = getSelectedPlay();
+  moves ++;
+  
+  if (moveComputer === movePlayer){
+    result.innerHTML = 'Empate!'
+  } else if (movePlayer === "rock") {
+    if (moveComputer === "paper") {
+        result.innerHTML = "Has perdido!"
+        computerScore++;
+    } else if (moveComputer === "scissors") {
+        result.innerHTML = "Has ganado!"
+        playerScore++;
+    }
+
+} else if (movePlayer === "scissors") {
+    if (moveComputer === "rock") {
+        result.innerHTML = "Has perdido!"
+        computerScore++;
+    } else if (moveComputer === "paper") {
+        result.innerHTML = "Has ganado!"
+        playerScore++;
+    }
+
+} else if (movePlayer === "paper") {
+    if (moveComputer === "scissors") {
+        result.innerHTML = "Has perdido!"
+        computerScore++;
+    } else if (moveComputer === "rock") {
+        result.innerHTML = "Has ganado!"
+        playerScore++;
+    }
+}
+computer.innerHTML = "Ordenador: " + computerScore;
+player.innerHTML = "Jugador: "+playerScore;
+}
+
+//Funcion de finalizar juego
+function gameOver() {
+  if (moves === 10) {
+      if (playerScore > computerScore) {
+          result.innerHTML = "Has ganado el juego"
+      } else if (playerScore < computerScore) {
+          result.innerHTML = "Has perdido el juego"
+      } else {
+          result.innerHTML = "Empate"
+      }
+      playButton.classList.add("hidden");
+      resetButton.classList.remove("hidden");
   }
-
-  counter++;
 }
-//Falta Funcion para contador
-
-function handleClickPlay(event) {
-  event.preventDefault();
-  let optionPc = getOptionComputer(randomNumber);
-
- player.textContent = selectPlay.value;
- computer.textContent = optionPc;
- compareOptionPlayer(optionPc);
+//Funcion para mostrar los resultados
+function hadleClickPlayGame(ev) {
+  ev.preventDefault();
+  playGame();
+  gameOver();
 }
 
-//Listeners
+//Funcion para empezar otra vez el juego
+function handleClickReload(ev) {
+  ev.preventDefault();
+  playButton.classList.remove("hidden");
+  resetButton.classList.add("hidden");
+  playerScore = 0;
+  computerScore = 0;
+  moves = 0;
+  result.innerHTML = "Vamos a jugar"
+  computer.innerHTML = "Ordenador: " + computerScore;
+  player.innerHTML = "Jugador:  " + playerScore;
+}
 
-playButton.addEventListener("click", handleClickPlay);
+//Eventos 
+playButton.addEventListener('click', hadleClickPlayGame);
+resetButton.addEventListener('click', handleClickReload)
